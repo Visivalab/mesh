e21800228e3a40120e9bf2af9556a3175ee186dc
GerardEst <gesteve.12@gmail.com>
2021-04-09 18:18:53 +0200

    Admin - Edit projects

--- public/js/admin.js
+++ public/js/admin.js
@@ -3,6 +3,47 @@ console.log("ADMIN")
 
 let projectTable = document.querySelector('#projectTable tbody')
 
+
+const modal = (function(){
+
+  const modalBox = document.querySelector('.modal')
+  const modalContent = modalBox.querySelector('.modal-content')
+  // Init interactions
+  function init(){
+    modalBox.querySelector('.modal-close').addEventListener( 'click', remove )
+  }
+  
+  const open = () => modalBox.classList.add('is-active')
+  const remove = () => modalBox.classList.remove('is-active')
+  
+  const loadContent = async project => {
+    modalBox.setAttribute('project', project)
+    modalBox.querySelector('.project-title').textContent = project
+
+    const projectInfo_call = await fetch('/api/project/'+project)
+    const projectInfo = await projectInfo_call.json()
+
+    
+
+    for(let mesh of projectInfo.meshes){
+      const dom_mesh = document.createElement('div')
+      dom_mesh.className = 'project__mesh'
+      dom_mesh.innerHTML = `
+        <p>${mesh.name}</p>
+        <input type="file" class="button is-success">
+        <a class="button is-success" href="${mesh.url}">Download</a>
+      `
+      modalContent.querySelector('.project-mesh-list').appendChild(dom_mesh)
+    }
+
+    
+  }
+  init()
+
+  return{ open, remove, loadContent }
+})()
+
+
 fetch('/api/projects')
 .then( response => response.json() )
 .then( projects => {
@@ -17,8 +58,12 @@ fetch('/api/projects')
     projectName.textContent = project.title
     projectId.innerHTML = `<a target="_blank" href=/view/${project._id}>${project._id}</a>`
     projectEasyId.innerHTML = `<a target="_blank" href=/view/${project.easyId}>${project.easyId}</a>`
-    projectOptions.innerHTML = "<button class='button'>Editar</button>"
+    projectOptions.innerHTML = `<button class='button'>Editar</button>`
    
+    projectOptions.addEventListener('click', () => {
+      modal.open()
+      modal.loadContent(project._id)
+    })
 
     projectRow.appendChild(projectName)
     projectRow.appendChild(projectEasyId)
